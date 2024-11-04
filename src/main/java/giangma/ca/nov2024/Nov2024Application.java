b55c8bbe9ce6623ed450bc0febf827244c586f4f
Manh Phu Giang <gmanhphuu@gmail.com>
2024-11-03 22:30:00 -0500

    Init

---
+++ src/main/java/giangma/ca/nov2024/Nov2024Application.java
@@ -0,0 +1,13 @@
+package giangma.ca.nov2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nov2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nov2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/giangma/ca/nov2024/Nov2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package giangma.ca.nov2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nov2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
