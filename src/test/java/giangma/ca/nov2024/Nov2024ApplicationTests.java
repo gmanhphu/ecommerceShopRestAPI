92e29e47732feda3d10d7f0709e44f7cc60095c1
Manh Phu Giang <gmanhphuu@gmail.com>
2024-11-19 17:02:10 -0500

    minor change

--- src/test/java/giangma/ca/nov2024/Nov2024ApplicationTests.java
+++ src/test/java/giangma/ca/nov2024/Nov2024ApplicationTests.java
@@ -1,13 +1,18 @@
 package giangma.ca.nov2024;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
 @SpringBootTest
 class Nov2024ApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
