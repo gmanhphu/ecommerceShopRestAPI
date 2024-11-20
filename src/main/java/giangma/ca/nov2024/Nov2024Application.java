e4e48c98f51958df3a6f20e461d57de22577cc7b
Manh Phu Giang <gmanhphuu@gmail.com>
2024-11-19 20:15:27 -0500

    anotation problem fixed

--- src/main/java/giangma/ca/nov2024/Nov2024Application.java
+++ src/main/java/giangma/ca/nov2024/Nov2024Application.java
@@ -1,17 +1,12 @@
 package giangma.ca.nov2024;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-@Configuration
 public class Nov2024Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Nov2024Application.class, args);
     }
-
 }
